d29b241950ce994cbad7d6779a5928a22ef45f55
huanghuanjun <junqing124@126.com>
2020-06-10 17:29:38 +0800

    错误日志

--- app/Model/MyError.php
+++ app/Model/MyError.php
@@ -18,24 +18,23 @@ class MyError extends \Whoops\Handler\Handler
         // TODO: Implement handle() method.
         //系统日志
 
-        $clsConfig = new Config(CONFIG_DIR .DS. 'log.php');
-        $clsConfig->setDriver('php');
-        $clsConfig->init();
-        $logHandler = $clsConfig->get('log.error_driver');
-
-        $clsSystemLogger = new SystemLogger(CONFIG_DIR . DS . 'log.php'); //配置文件
-        $clsSystemLogger->addHandler($logHandler);
-        $clsSystemLogger->setLogInfo(
-            array(
+        try {
+            $clsSystemLogger = new SystemLogger(CONFIG_DIR . DS . 'log.php'); //配置文件
+            $clsSystemLogger->addHandler(config('log.error_driver'));//添加额外的处理
+            $clsSystemLogger->setTitle($this->getException()->getMessage());
+            $logInfo = array(
                 'ack' => 0, //1或0 这是成功还是失败
                 'level' => 'critical', //warning info debug notice critical emergency
                 'add_time' => date('Y-m-d H:i:s'),
                 'message' => $this->getException()->getMessage(),
                 'source' => 'dcrphp-error',
-                'line'=> $this->getException()->getLine(),
-                'file'=> $this->getException()->getFile(),
-            )
-        );
-        $clsSystemLogger->critical();
+                'line' => $this->getException()->getLine(),
+                'file_name' => $this->getException()->getFile(),
+            );
+            $clsSystemLogger->setLogInfo($logInfo);
+            $clsSystemLogger->critical();
+        } catch (\Exception $e) {
+            throw $e;
+        }
     }
 }
--- config/log.php
+++ config/log.php
@@ -3,31 +3,36 @@
  * 日志分为用户日志和系统日志(系统日志和错误日志)，
  */
 return array(
-    'channel' => 'dcrphp',   //频道名 一般定义为系统名    'driver'=>
+    'channel' => env('LOG_CHANNEL', ''),   //频道名 一般定义为系统名    'driver'=>
     'system_driver' => env('LOG_DRIVER', ''), //@log注解或手动记录应用的日志
     'error_driver' => env('LOG_DRIVER_ERROR', ''), //系统捕获的error日志
 
-    'file' => array('path' => 'storage/log.log'),
+    'file' => array('path' => env('FILE_LOG_PATH', '')),
     //directory为日志生成在path目录下， general为day则按天 hour按时 month按月 minute按分，prefix为日志文件后缀默认为log
-    'directory' => array('path' => 'storage/log', 'prefix' => 'php', 'general' => 'hour'),
+    'directory' => array(
+        'path' => env('DIRECTORY_LOG_PATH', ''),
+        env('DIRECTORY_LOG_PREFIX', ''),
+        env('DIRECTORY_LOG_GENERAL', '')
+    ),
     //如果要用户日志，则一定要配置这个
     'mongodb' => array(
-        'host' => 'ip',
-        'port' => 'port',
-        'database' => 'database',
-        'collection' => 'collection',
-        'username'=> '',
-        'password'=> '',
+        'host' => env('MONGODB_LOG_HOST', ''),
+        'port' => env('MONGODB_LOG_PORT', ''),
+        'database' => env('MONGODB_LOG_DATABASE', ''),
+        'collection' => env('MONGODB_LOG_COLLECTION', ''),
+        'username' => env('MONGODB_LOG_USERNAME', ''),
+        'password' => env('MONGODB_LOG_PASSWORD', ''),
     ),
     'redis' =>
         array(
-            'host' => 'host',
-            'port' => 'port',
-            'password' => 'password',
-            'key' => 'log',
+            'host' => env('REDIS_LOG_HOST', ''),
+            'port' => env('REDIS_LOG_PORT', ''),
+            'password' => env('REDIS_LOG_PASSWORD', ''),
+            'key' => env('REDIS_LOG_KEY', ''),
         ),
     //要用graylog,则要引入:graylog2/gelf-php.
     //这个ip和port请在graylog中的 System/inputs->inputs 新建一个gelf udp就OK了，记得端口配置在下面
-    'graylog' => array('host' => '10.10.30.217', 'port' => '12201'),
+    'graylog' => array('host' => env('GRAYLOG_LOG_HOST', ''), 'port' => env('GRAYLOG_LOG_PORT', '')),
+    //下面的type为任意值
     'browser' => array('type' => 'browser'),
 );
