a6b07dbaadae63f9bef85f0e9e6b71de73107ad1
huanghuanjun <junqing124@126.com>
2020-07-22 20:34:53 +0800

    添加了数据库管理插件

--- app/Admin/Plugins/DbManager/Controller/DbManager.php
+++ app/Admin/Plugins/DbManager/Controller/DbManager.php
@@ -3,11 +3,39 @@
 namespace app\Admin\Plugins\DbManager\Controller;
 
 use app\Admin\Model\Plugins;
+use dcr\facade\Db;
 
 class DbManager extends Plugins
 {
 
     public function execute($view)
     {
+        $sql = post('sql');
+        $type = post('type');
+        $functionName = 'query';
+        if ('not_select'==$type) {
+            $functionName = 'exec';
+        }
+        $result = Db::$functionName($sql);
+
+        if ($result && 'select'==$type) {
+            $list = $result;
+            $result = '<table class="table table-border table-bordered table-hover table-bg table-sort">
+            <thead>
+            <tr>';
+            $cols = array_keys($list[0]); //列名
+            foreach ($cols as $col) {
+                $result .= "<th width=\"100\">{$col}</th>";
+            }
+            $result .= '</tr></thead>';
+            $result .= '<tbody>';
+            foreach ($list as $value) {
+                foreach ($cols as $col) {
+                    $result .= "<td>{$value[$col]}</td>";
+                }
+            }
+            $result .= '</tbody></table>';
+        }
+        return array('ack' => 1, 'data' => $result);
     }
 }
