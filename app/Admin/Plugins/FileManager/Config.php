c2e5492e648d67781b97a919e654bd2cceaf56cc
huanghuanjun <junqing124@126.com>
2020-08-16 22:57:55 +0800

    Update Config.php

--- app/Admin/Plugins/FileManager/Config.php
+++ app/Admin/Plugins/FileManager/Config.php
@@ -1,8 +1,9 @@
 <?php
+
 return array(
-    'name'=> 'FileManager', //与目录名相同
-    'description'=> '文件管理',
-    'title'=> '文件管理', //菜单栏上显示的
-    'author'=> 'dcr',
-    'version'=> '1.0.1',
+    'name' => 'FileManager', //与目录名相同
+    'description' => '文件管理',
+    'title' => '文件管理', //菜单栏上显示的
+    'author' => 'dcr',
+    'version' => '1.0.1',
 );
